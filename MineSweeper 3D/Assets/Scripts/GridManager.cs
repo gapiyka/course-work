f883f558037ae7920ef7bcaddd40c6e6d016c478
gapiyka <50524296+gapiyka@users.noreply.github.com>
2021-12-13 15:57:07 +0200

    add mouse_control + tiles_activation

--- MineSweeper 3D/Assets/Scripts/GridManager.cs
+++ MineSweeper 3D/Assets/Scripts/GridManager.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
-struct Tile
+public struct Tile
 {
-    public int counter;
     public GameObject gameObj;
+    public int minesCounter;
+    public bool IsChecked;
+    public bool IsFlag;
 }
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private GameObject minePrefab;
+    [SerializeField] private GameObject flagPrefab;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private Transform groundTransform;
     [SerializeField] private GameObject[] tilesPrefabs;
@@ -19,11 +21,12 @@ public class GridManager : MonoBehaviour
     private float mapMiddle; // center of map (used to start point)
     private int mines—hance; // chance of generation mines on Map
     private int nMines; // number of mines at map
-    private Tile[,] gridMatrix; // two-dimensional array to detect mines
 
     private const int sizeMultiplier = 9; // multipler for payable size
     private const int minesMultiplier = 20; // diviner to count n of mines
     private const int tileSize = 5; // size of plate from plane object scale
+
+    public Tile[,] gridMatrix; // two-dimensional array to detect mines
 
     void Start()
     {
@@ -39,7 +42,7 @@ public class GridManager : MonoBehaviour
         mapSize = gridSize * gridSize;
         mapMiddle = gridSize / 2f;
 
-        playerTransform.position = GetStartPos(1.1f);
+        playerTransform.position = GetStartPos(1.1f); // spawn palyer at middle of map
 
         GenerateGrid();
     }
@@ -47,8 +50,8 @@ public class GridManager : MonoBehaviour
     // generation grid with mines
     void GenerateGrid()
     {
-        const int lowerBorder = 0;
-        const int upperBorder = 100;
+        const int randomLowerBorder = 0;
+        const int randomUpperBorder = 100;
         int randomChance;
 
         nMines = 0;
@@ -59,17 +62,18 @@ public class GridManager : MonoBehaviour
         {
             for (int z = 0; z < gridSize; z++)
             {
-                randomChance = UnityEngine.Random.Range(lowerBorder, upperBorder);
+                randomChance = UnityEngine.Random.Range(randomLowerBorder, randomUpperBorder);
                 if (randomChance < mines—hance)
                 {
                     gridMatrix[x, z].gameObj = Instantiate(tilesPrefabs[9], new Vector3(x * tileSize + tileSize / 2, 0, z * tileSize + tileSize / 2f), Quaternion.identity, this.transform);
-                    gridMatrix[x, z].counter = 9;
+                    gridMatrix[x, z].minesCounter = 9;
                     nMines++;
                 }
+                gridMatrix[x, z].IsChecked = false; // set tile not checked
+                gridMatrix[x, z].IsFlag = false; // set tile without flag
             }
         }
         PaintMineArea();
-        Debug.Log(nMines);
     }
 
     //should be optimized
@@ -80,38 +84,38 @@ public class GridManager : MonoBehaviour
             for (int z = 0; z < gridSize; z++)
             {
                 int n = 0;
-                if (gridMatrix[x, z].counter == 9) continue;
+                if (gridMatrix[x, z].minesCounter == 9) continue;
                 if(x != 0)
                 {
-                    if (gridMatrix[x - 1, z].counter == 9) n++;
+                    if (gridMatrix[x - 1, z].minesCounter == 9) n++;
                 }
                 if (z != 0)
                 {
-                    if (gridMatrix[x, z - 1].counter == 9) n++;
+                    if (gridMatrix[x, z - 1].minesCounter == 9) n++;
                 }
                 if (x != 0 && z != 0)
                 {
-                    if (gridMatrix[x - 1, z - 1].counter == 9) n++;
+                    if (gridMatrix[x - 1, z - 1].minesCounter == 9) n++;
                 }
                 if (x != gridSize - 1)
                 {
-                    if (gridMatrix[x + 1, z].counter == 9) n++;
+                    if (gridMatrix[x + 1, z].minesCounter == 9) n++;
                 }
                 if (z != gridSize - 1)
                 {
-                    if (gridMatrix[x, z + 1].counter == 9) n++;
+                    if (gridMatrix[x, z + 1].minesCounter == 9) n++;
                 }
                 if (x != gridSize - 1 && z != gridSize - 1)
                 {
-                    if (gridMatrix[x + 1, z + 1].counter == 9) n++;
+                    if (gridMatrix[x + 1, z + 1].minesCounter == 9) n++;
                 }
                 if (x != 0 && z != gridSize - 1)
                 {
-                    if (gridMatrix[x - 1, z + 1].counter == 9) n++;
+                    if (gridMatrix[x - 1, z + 1].minesCounter == 9) n++;
                 }
                 if (x != gridSize - 1 && z !=0)
                 {
-                    if (gridMatrix[x + 1, z - 1].counter == 9) n++;
+                    if (gridMatrix[x + 1, z - 1].minesCounter == 9) n++;
                 }
 
                 gridMatrix[x, z].gameObj = Instantiate(tilesPrefabs[n], new Vector3(x * tileSize + tileSize / 2, 0f, z * tileSize + tileSize / 2f), Quaternion.identity, this.transform); ;
@@ -124,8 +128,47 @@ public class GridManager : MonoBehaviour
         return new Vector3(tileSize * mapMiddle, height, tileSize * mapMiddle);
     }
 
-    public int GetMinesNumber()
+    void SwitchTileState(Tile tile, int button, int xInArray, int zInArray)
     {
-        return nMines;
+        bool IsAlreadyChecked = tile.IsChecked;
+        bool IsAlreadyFlag = tile.IsFlag;
+        Transform CheckerTransform = tile.gameObj.transform.GetChild(0);
+        Transform BodyTransform = tile.gameObj.transform.GetChild(1);
+
+        if (!IsAlreadyChecked)
+        {
+            if (button == 1 && !IsAlreadyFlag)
+            {
+                CheckerTransform.gameObject.SetActive(false);
+                BodyTransform.gameObject.SetActive(true);
+                gridMatrix[xInArray, zInArray].IsChecked = true;
+            }
+
+            if (button == 2)
+            {
+                if (IsAlreadyFlag)
+                {
+                    Destroy(CheckerTransform.Find("Flag(Clone)").gameObject);// can be changed on SetActive
+                    gridMatrix[xInArray, zInArray].IsFlag = false;
+                }
+                else
+                {
+                    Instantiate(flagPrefab, CheckerTransform.position, Quaternion.identity, CheckerTransform);
+                    gridMatrix[xInArray, zInArray].IsFlag = true;
+                }
+            }
+        }   
+    }
+
+    public void FindPointedTile(GameObject obj, int button)
+    {
+        for (int x = 0; x < gridMatrix.GetLength(0); x++)
+        {
+            for (int z = 0; z < gridMatrix.GetLength(1); z++)
+            {
+                Tile tile = gridMatrix[x, z];
+                if (tile.gameObj.transform.position == obj.transform.position) SwitchTileState(tile, button, x, z);
+            }
+        }   
     }
 }
--- MineSweeper 3D/Assets/Scripts/PlayerController.cs
+++ MineSweeper 3D/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace PlayerConfiguration
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private GameObject camera;
+        [SerializeField] private Camera camera;
         [SerializeField] private GameObject player;
         [SerializeField] private CharacterController controller;
         [SerializeField] private Transform groundCheck;
+        [SerializeField] private Transform pointer;
         [SerializeField] private LayerMask groundMask;
+        [SerializeField] private GridManager gridManager;
 
         public float mouseSensitivity = 100f;
         public float speed = 8f;
@@ -20,7 +20,9 @@ namespace PlayerConfiguration
         private float groundDistance = 0.3f;
         private float yRotation = 0f;
         private Vector3 velocity;
-        private bool IsGrounded;
+        private bool IsGrounded = true;
+        private bool IsPressedLMB = false;
+        private bool IsPressedRMB = false;
 
         void Start()
         {
@@ -40,13 +42,35 @@ namespace PlayerConfiguration
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
             float axisX = Input.GetAxis("Horizontal");
             float axisZ = Input.GetAxis("Vertical");
+            IsPressedLMB = Input.GetButtonUp("Fire1"); // detect pressed button
+            IsPressedRMB = Input.GetButtonUp("Fire2"); // detect pressed button
 
             Vector3 move = player.transform.right * axisX + player.transform.forward * axisZ;
             yRotation -= mouseY;
             yRotation = Mathf.Clamp(yRotation, -90f, 90f);
-            camera.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+            camera.gameObject.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+
             player.transform.Rotate(Vector3.up * mouseX);
             controller.Move(move * speed * Time.deltaTime);
+            if (IsPressedLMB || IsPressedRMB) OnButtonPressed();
+        }
+
+        void OnButtonPressed()
+        {
+            const int tileDistance = 3;
+            RaycastHit hit;
+            Vector3 cameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, camera.nearClipPlane));
+            int button = IsPressedRMB ? 2 : 1;
+
+            
+            if (Physics.Raycast(cameraCenter, camera.gameObject.transform.forward, out hit, tileDistance))
+            {
+                GameObject obj = hit.transform.gameObject;
+                gridManager.FindPointedTile(obj, button);
+            }
+
+            IsPressedLMB = false;
+            IsPressedRMB = false;
         }
     }
 }
