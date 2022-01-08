ba15b06c5670aeff63d71a8638e91753212264ce
Artem <bondarchuk.artem20@gmail.com>
2022-01-08 16:32:02 +0200

    Added tests

--- MineSweeper 3D/Assets/Tests/EditMode/GridManager_Test.cs
+++ MineSweeper 3D/Assets/Tests/EditMode/GridManager_Test.cs
@@ -8,20 +8,43 @@ public class GridManager_Test
     {
         var gridObj = new GameObject();
         var gridManager = gridObj.AddComponent<GridManager>();
-        int parameter1 = 1;
-        int parameter2 = 2;
-        int parameter3 = 3;
-        Vector3 expectedResult1 = new Vector3(parameter1, 1, parameter1);
-        Vector3 actualResult1 = gridManager.CalcBorderScale(parameter1);
+        const int difficulty1 = 1;
+        const int difficulty2 = 2;
+        const int difficulty3 = 3;
+        Vector3 expectedResult1 = new Vector3(difficulty1, 1, difficulty1);
+        Vector3 actualResult1 = gridManager.CalcBorderScale(difficulty1);
         Assert.AreEqual(expectedResult1, actualResult1);
 
-        Vector3 expectedResult2 = new Vector3(parameter2, 1, parameter2);
-        Vector3 actualResult2 = gridManager.CalcBorderScale(parameter2);
+        Vector3 expectedResult2 = new Vector3(difficulty2, 1, difficulty2);
+        Vector3 actualResult2 = gridManager.CalcBorderScale(difficulty2);
         Assert.AreEqual(expectedResult2, actualResult2);
 
-        Vector3 expectedResult3 = new Vector3(parameter3, 1, parameter3);
-        Vector3 actualResult3 = gridManager.CalcBorderScale(parameter3);
+        Vector3 expectedResult3 = new Vector3(difficulty3, 1, difficulty3);
+        Vector3 actualResult3 = gridManager.CalcBorderScale(difficulty3);
         Assert.AreEqual(expectedResult3, actualResult3);
 
+    }
+    
+    [Test]
+    public void CalcBorderPos_Test()
+    {
+        var gridObj = new GameObject();
+        var gridManager = gridObj.AddComponent<GridManager>();
+        const int gridSize1 = 9;
+        const int gridSize2 = 17;
+        const int gridSize3 = 25;
+        const int tileSize = 5;
+
+        Vector3 expectedResult1 = new Vector3(gridSize1 * tileSize, 0, gridSize1 * tileSize);
+        Vector3 actualResult1 = gridManager.CalcBorderPos(gridSize1, tileSize);
+        Assert.AreEqual(expectedResult1, actualResult1);
+
+        Vector3 expectedResult2 = new Vector3(gridSize2 * tileSize, 0, gridSize2 * tileSize);
+        Vector3 actualResult2 = gridManager.CalcBorderPos(gridSize2, tileSize);
+        Assert.AreEqual(expectedResult2, actualResult2);
+
+        Vector3 expectedResult3 = new Vector3(gridSize3 * tileSize, 0, gridSize3 * tileSize);
+        Vector3 actualResult3 = gridManager.CalcBorderPos(gridSize3, tileSize);
+        Assert.AreEqual(expectedResult3, actualResult3);
     }
 }
